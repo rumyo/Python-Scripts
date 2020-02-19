805e96e53f229a9c83851ca57a57e1d3f93b0f4b
rumyo <luis.lance25@gmail.com>
2020-02-19 04:12:41 -0500

    Updated loop3.py

--- loop3.py
+++ loop3.py
@@ -2,6 +2,17 @@ loop = True
 data = {}
 
 while loop:
-    age = int(input('ingrese edad'))
+    try:
+       age = int(input('ingrese edad \n'))
+    except ValueError:
+        print('Error, ingrese un numero valido')
+        continue
     if age > 18:
         print('eres mayor')
+    else:
+        print('Eres menor')
+
+    r = input('Quiere volver a intentar? s/n \n')
+    if r == 'n':
+        print('Usted ha cerrado el programa')
+        loop = False
