bf07b38565c892aaa453bfd380d9665c88897c52
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-15 01:51:28 -0500

    Update enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -2,6 +2,6 @@ winners = ["Ashley", "Dylan", "Reese"]
 for index, person in enumerate(winners):
   print("{} - {}".format(index + 1, person))
   
-  // 1 - Ashley
-  // 2 - Dylan
-  // 3 - Reese
+# 1 - Ashley
+# 2 - Dylan
+# 3 - Reese
