44c5858976432b13da451ecc3d0e98474db8c327
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-15 01:49:40 -0500

    Create enumerate2.py

---
+++ enumerate2.py
@@ -0,0 +1,10 @@
+def skip_elements(elements):
+  elements = [v for i, v in enumerate(elements) if i % 2 == 0]
+
+  return elements
+
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) 
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) 
+
+# ['a', 'c', 'e', 'g']
+# ['Orange', 'Strawberry', 'Peach']
