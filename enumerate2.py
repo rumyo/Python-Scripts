879c321d20f0ce53ad6cf53c548b7e2899842d21
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-15 01:53:51 -0500

    Update enumerate2.py

--- enumerate2.py
+++ enumerate2.py
@@ -6,5 +6,6 @@ def skip_elements(elements):
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) 
 print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach'])) 
 
+# Print values with even index
 # ['a', 'c', 'e', 'g']
 # ['Orange', 'Strawberry', 'Peach']
