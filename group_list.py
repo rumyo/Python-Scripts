a9ef711b11bc16e7ced71cf44d109fa11d2d45e6
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-16 01:25:09 -0500

    Create group_list.py

---
+++ group_list.py
@@ -0,0 +1,7 @@
+def group_list(group, users):
+  members = ', '.join(users)
+  return "{}: {}".format(group, members)
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
