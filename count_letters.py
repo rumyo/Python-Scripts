4ce69e7065bf61f5cd767c9796555cf6c40f98d6
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-19 17:39:59 -0500

    Create count_letters.py

---
+++ count_letters.py
@@ -0,0 +1,23 @@
+#Counts the letters, ignoring everything else
+
+def count_letters(text):
+  result = {}
+  text = text.lower()
+  # Go through each letter in the text
+  for letter in text:
+   
+    # Check if the letter needs to be counted or not
+    if letter.isalpha() :
+      # Add or increment the value in the dictionary
+      count = text.count(letter)
+      result[letter] = count
+  return result
+
+print(count_letters("AaBbCc"))
+# Should be {'a': 2, 'b': 2, 'c': 2}
+
+print(count_letters("Math is fun! 2+2=4"))
+# Should be {'m': 1, 'a': 1, 't': 1, 'h': 1, 'i': 1, 's': 1, 'f': 1, 'u': 1, 'n': 1}
+
+print(count_letters("This is a sentence."))
+# Should be {'t': 2, 'h': 1, 'i': 2, 's': 3, 'a': 1, 'e': 3, 'n': 2, 'c': 1}
