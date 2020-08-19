afd4249d1e0a6d541075ebdf010b3f9bc0bed0f0
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-19 16:29:56 -0500

    Create highlight_word.py

---
+++ highlight_word.py
@@ -0,0 +1,11 @@
+def highlight_word(sentence, word):
+	return(sentence.replace(word,word.upper()))
+
+print(highlight_word("Have a nice day", "nice"))
+print(highlight_word("Shhh, don't be so loud!", "loud"))
+print(highlight_word("Automating with Python is fun", "fun"))
+
+'''
+Have a NICE day
+Shhh, don't be so LOUD!
+Automating with Python is FUN'''
