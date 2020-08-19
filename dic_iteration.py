847974c15d9a492b27b2ba16c1510bfe7637108e
rumyo <55766920+rumyo@users.noreply.github.com>
2020-08-19 01:11:17 -0500

    Update dic_iteration.py

--- dic_iteration.py
+++ dic_iteration.py
@@ -2,7 +2,8 @@ wardrobe = {"shirt":["red","blue","white"], "jeans":["blue","black"]}
 for key, value in wardrobe.items():
     for i in value:
         print("{} {}".format(value, key))
-'''       
+        
+'''
 Here is your output:
 red shirt
 blue shirt
