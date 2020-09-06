47cc37488a56f40fccc7ba646b2dcf118fdd4f69
rumyo <55766920+rumyo@users.noreply.github.com>
2020-09-06 01:10:06 -0500

    Create contruct.py

---
+++ contruct.py
@@ -0,0 +1,9 @@
+class Apple:
+     def __init__(self, color, flavor):
+         self.color = color
+         self.flavor = flavor
+         
+jonagold = Apple("red", "sweet")
+print(jonagold.color)
+    
+# Prints Red
