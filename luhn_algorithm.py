4dcbdf7e26ccab1ca8a871bd8259c808364d7393
rumyo <55766920+rumyo@users.noreply.github.com>
2020-11-02 00:01:34 -0500

    Create luhn_algorithm.py

---
+++ luhn_algorithm.py
@@ -0,0 +1,20 @@
+def luhn(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    return (checksum % 10)
+
+#A little code to test it out on a number
+if (luhn("8830723086640477")):  #this is a valid number
+    print("INVALID")
+else:
+    print("VALID")
+    #print(luhn("8830723086640477"));
+
+#result will print valid
