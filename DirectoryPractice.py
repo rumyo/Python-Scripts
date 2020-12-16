8eeacf328d79cdd67bed477855b6aa9d5436d62b
rumyo <55766920+rumyo@users.noreply.github.com>
2020-12-16 18:51:35 -0500

    Create DirectoryPractice.py

---
+++ DirectoryPractice.py
@@ -0,0 +1,19 @@
+import os
+
+def new_directory(directory, filename):
+  # Before creating a new directory, check to see if it already exists
+  if os.path.isdir(directory) == False:
+    os.mkdir(directory)
+
+  # Create the new file inside of the new directory
+  os.chdir(directory) #change to new directory
+  with open (filename, "w") as file:
+    pass
+
+  # Return the list of files in the new directory
+  return os.listdir(os.getcwd())
+  print(new_directory("PythonPrograms", "script.py"))
+
+print(new_directory("PythonPrograms", "script.py"))
+
+#output is: ['script.py']
